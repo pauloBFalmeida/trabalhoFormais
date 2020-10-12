d54352c17e65b70e1ec1458bd76cd31327b2ba9c
Paulo B F Almeida <paulobfalmeida@gmail.com>
2020-10-12 20:54:52 -0300

    afnd textos

--- afnd.py
+++ afnd.py
@@ -6,55 +6,49 @@ class AFND(AFD):
         super().__init__(estados, alfabeto, estadoInicial, estadosFinais, transicoes)
         self.alfabeto.add("&")
 
-        self.epsilonFecho = {}
+    #    self.epsilonFecho = {}
 
+    #def epsilonFechoFunc(self, estado):
+    #    self.epsilonFecho[estado] = set([estado])
+    #    current = [estado]
+    #    visited = set()
 
-
-    def epsilonFechoFunc(self, estado):
-        self.epsilonFecho[estado] = set([estado])
-        current = [estado]
-        visited = set()
-
-        while len(current)>0:
-            next = []
-            for s in current:
-                if '&' in self.transicoes[s]:
-                    for t in self.transicoes[s]['&']:
-                        if not t in visited:
-                            next.append(t)
-                            visited.append(t)
-                        self.epsilonFecho[estado].add(t)
-            current = next
-        return
-
+    #    while len(current)>0:
+    #        next = []
+    #        for s in current:
+    #            if '&' in self.transicoes[s]:
+    #                for t in self.transicoes[s]['&']:
+    #                    if not t in visited:
+    #                        next.append(t)
+    #                        visited.append(t)
+    #                    self.epsilonFecho[estado].add(t)
+    #        current = next
+    #    return
 
     def computar(self, s):
-        for t in self.estados:
-            self.epsilonFechoFunc(t)
-
-        estadosAtuais = self.epsilonFecho[self.estadoInicial]
-
+        estadosAtuais = set([self.estadoInicial])
         for c in s:
+            # simbolo nao pertecente ao alfabeto
             if c not in self.alfabeto:
                 raise SimboloInexistente(str(c))
-
-            proximosEstados = set()
-
-            for e in estadosAtuais:
-                if c in self.transicoes[e]:
-                    print(self.epsilonFecho)
-                    print(self.transicoes[e][c])
-                    t = map(lambda x: self.epsilonFecho[x], \
-                                            self.transicoes[e][c])
-                    alguma = []
-                    for i in t:
-                        for _ in range(len(i)):
-                            alguma.append(i.pop())
-                    proximosEstados.union(set(alguma))
-
-            estadosAtuais = proximosEstados
-
-        for e in estadosAtuais:
-            if e in self.estadosFinais:
+            for ea in estadosAtuais:
+                # passo pelas epsilon transicoes
+                if '&' in self.transicoes[ea]:
+                    estadosAtuais.add(self.transicoes[ea]['&'])
+                # removo os estados com transicoes mortas
+                if c not in self.transicoes[ea]:
+                    estadosAtuais.remove(ea)
+            # nenhum estado restante com trasicoes validas
+            if len(estadosAtuais) == 0:
+                return False
+            # percorro as transicoes
+            estados = list(estadosAtuais)
+            estadosAtuais.clear()
+            for ea in estados:
+                estadosAtuais.update([ne for ne in self.transicoes[ea][c]])
+        # vejo se tem algum estado que seja de aceitacao
+        for ea in estadosAtuais:
+            if ea in self.estadosFinais:
                 return True
         return False
+
--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from afd import AFD
 from afnd import AFND
 
+def criarAFD(nEstados, alfabeto, estadoInicial, estadosFinais, transicoes):
+    afd = AFD([str(i) for i in range(int(nEstados))], alfabeto, estadoInicial, estadosFinais)
+    for t in transicoes:
+        afd.addTransicao(t[0],t[1],t[2])
+    return afd
+
+def criarAFND(nEstados, alfabeto, estadoInicial, estadosFinais, transicoes):
+    afnd = AFND([str(i) for i in range(int(nEstados))], alfabeto, estadoInicial, estadosFinais)
+    for t in transicoes:
+        for estadosFinais in t[2].split('-'):
+            afnd.addTransicao(t[0],t[1],estadosFinais)
+    return afnd
+
 def lerArquivoAF(arquivo):
     with open(arquivo, "r") as file:
         linhas = file.read().split('\n')
-        nEstados = linhas[0]
-        estadoInicial = linhas[1]
-        estadosFinais = linhas[2].split(',')
-        alfabeto = linhas[3].split(',')
-        transicoes = []
-        for linha in linhas[4:]:
-            transicoes.append(linha.split(','))
-    rodrigo = AFND([str(i) for i in range(int(nEstados))], alfabeto, estadoInicial, estadosFinais)
-    for t in transicoes:
-        rodrigo.addTransicao(t[0],t[1],t[2])
+
+    nEstados = linhas[0]
+    estadoInicial = linhas[1]
+    estadosFinais = linhas[2].split(',')
+    alfabeto = linhas[3].split(',')
+    transicoes = []
+    for linha in linhas[4:]:
+        transicoes.append(linha.split(','))
+
+    #rodrigo = criarAFD(nEstados, alfabeto, estadoInicial, estadosFinais, transicoes)
+    rodrigo = criarAFND(nEstados, alfabeto, estadoInicial, estadosFinais, transicoes)
 
     while(True):
         try:
